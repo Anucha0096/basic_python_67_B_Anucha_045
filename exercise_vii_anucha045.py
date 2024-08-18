5c94b8f84a94e91ebf1b8834dbea3b6699262fa6
Anucha0096 <notpkvc6844@gmail.com>
2024-08-18 17:37:52 +0700

    Exercise

---
+++ exercise_vii_anucha045.py
@@ -0,0 +1,15 @@
+numbers = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+odd = []
+even = []
+
+for number in numbers:
+  if number % 2 == 0:
+    even.append(number)
+  else :
+    odd.append(number)
+
+print ("เลขคู่ใน List:", even)
+print ("เลขคี่ใน List:", odd)
+
+#นายอนุชา ประชุมพันธุ์ ทส. 67/2 6749010045
+ 
